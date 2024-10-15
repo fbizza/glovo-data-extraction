9da2c36470e77edd5013c632e0c754631dac6cbc
Francesco Bizzarri <francesco.bizzarri@glovoapp.com>
2024-10-15 20:48:28 +0200

    Update promocodes query

--- promos/number_of_inserted_promocodes.sql
+++ promos/number_of_inserted_promocodes.sql
@@ -1,44 +1,3 @@
-SELECT
-   count(promocode_uses_id) as insertions
-FROM "delta".growth_discounts_odp.discounts_promocode_uses pu
-         LEFT JOIN delta.growth_discounts_odp.discounts_promocodes pc
-                   ON pc.promocode_id = pu.promocode_id
-WHERE pc.promotion_id in (32042682,31184592)
-
-
-
-WITH orders AS (
-    SELECT
-        promocode_promotion_id,
-        COUNT(DISTINCT o.order_id) AS orders,
-        COUNT(DISTINCT CASE WHEN order_total_purchase_local < 40 THEN o.order_id ELSE NULL END) AS less_than_40,
-        COUNT(DISTINCT CASE WHEN order_total_purchase_local >= 40 AND order_total_purchase_local < 50 THEN o.order_id ELSE NULL END) AS in_40_50,
-        COUNT(DISTINCT CASE WHEN order_total_purchase_local >= 50 AND order_total_purchase_local < 60 THEN o.order_id ELSE NULL END) AS in_50_60,
-        COUNT(DISTINCT CASE WHEN order_total_purchase_local >= 60 THEN o.order_id ELSE NULL END) AS more_than_60
-    FROM
-        delta.central_order_descriptors_odp.order_descriptors_v2 o
-    LEFT JOIN
-        delta.growth_pricing_discounts_odp.pricing_discounts p
-    ON
-        p.order_id = o.order_id
-    WHERE
-        order_country_code = 'PL'
-        AND o.order_final_status = 'DeliveredStatus'
-        AND o.order_parent_relationship_type IS NULL
-        AND promocode_promotion_id IN (32042682, 31184592, 28205777, 30602898)
-    GROUP BY
-        promocode_promotion_id
-),
-insertions as (
-    SELECT pc.promotion_id, pc.promocode_id,
-       COUNT(promocode_uses_id) AS insertions --conta il numero di clienti distinti che lo hanno messo
-    FROM delta.growth_discounts_odp.discounts_promocode_uses pu
-             LEFT JOIN delta.growth_discounts_odp.discounts_promocodes pc
-                       ON pc.promocode_id = pu.promocode_id
-    WHERE pc.promotion_id IN (32042682, 31184592, 28205777, 30602898)
-    GROUP BY 1, 2
-)
-SELECT o.*, i.* FROM orders o left join insertions i on o.promocode_promotion_id = i.promotion_id;
 
 --usa questa:
 SELECT
@@ -56,7 +15,19 @@ LEFT JOIN
     (SELECT DISTINCT customer_id FROM delta.central_order_descriptors_odp.order_descriptors_v2) o
     ON o.customer_id = a.customer_id
 WHERE
-    a.promocode_id IN (2580410773,2519344627, 2615170513, 2597604350)
-    --and a.customer_id = 152487369
+    a.promocode_id IN (2615170513, 2597604350)
 GROUP BY
 a.promocode_id
+
+-- order_ids associati ad un certo promocode:
+SELECT
+count(order_id)
+FROM
+    "delta"."growth_discounts_odp".discounts_promocode_uses a
+FULL OUTER JOIN
+    delta.growth_pricing_discounts_odp.pricing_discounts b
+    ON a.promocode_id = b.promocode_id
+    AND a.promocode_uses_id = b.promocode_use_id
+WHERE
+    a.promocode_id IN (2615170513, 2597604350)
+
